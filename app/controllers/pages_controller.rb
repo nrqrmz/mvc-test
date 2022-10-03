01501191a3c1b3da57b42f44f71379592321c15b
Enrique Ramirez <nrqrmz@gmail.com>
2022-10-03 18:35:13 -0500

    update search form to look for first name and last name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,23 @@ class PagesController < ApplicationController
   end
 
   def contact
-    @members = %w[alex betty cindy diane eric benjamin charly anthony arthur adele daniel ellen ephraim kate stephan].sort
+    # @members = %w[alex betty cindy diane eric benjamin charly anthony arthur adele daniel ellen ephraim kate stephan].sort
 
-    if params[:member]
-      @members = @members.select { |member| member.start_with?(params[:member])}
+    # if params[:member]
+    #   @members = @members.select { |member| member.start_with?(params[:member])}
+    # end
+
+    @members = [
+      { first: 'alexa', last: 'brewer' },
+      { first: 'bernardette', last: 'collins' },
+      { first: 'chloe', last: 'daniels' },
+      { first: 'diane', last: 'young' }
+    ]
+
+    if params[:first] || params[:last]
+      @members = @members.select do |member|
+        member[:first].start_with?(params[:first]) || member[:last].start_with?(params[:last])
+      end
     end
   end
 end
